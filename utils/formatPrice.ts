39857d9951ef2796e5dcb05c4c81bf18df105e3b
Matías Giménez <matiasgimenez.dev@gmail.com>
2024-01-10 16:14:31 -0300

    Update formatPrice.ts

--- utils/formatPrice.ts
+++ utils/formatPrice.ts
@@ -1,22 +1,7 @@
 export function formatPrice(price: number): string {
-	if (price === 0) return '0';
-	let formatedPrice = '';
-	let result: number;
-	let remainder: number = price;
-	while (remainder > 1000) {
-		result = Math.floor(remainder / 1000);
-		remainder = remainder % 1000;
-		if (result > 1000) {
-			formatedPrice = formatedPrice
-				? `${formatedPrice}.${formatPrice(result)}`
-				: `${formatPrice(result)}`;
-		} else {
-			formatedPrice = formatedPrice
-				? `${formatedPrice}.${result}`
-				: `${result}`;
-		}
-	}
-	formatedPrice = `${formatedPrice}.${remainder}`;
-
-	return formatedPrice;
+	return price.toLocaleString('es-ar', {
+		style: 'currency',
+		currency: 'ARS',
+		minimumFractionDigits: 0,
+	});
 }
